f73d38018e86f695453fbef14597884743c4abb6
Milco Numan <milco.numan@gmail.com>
2024-03-28 12:43:26 +0000

    Report uptime, not time since UNIX epoch

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ use psutil::memory;
 use redactr::load_rule_configs;
 use regex::Regex;
 use serde::Serialize;
-use std::time::{SystemTime, UNIX_EPOCH};
 use actix_web_prom::PrometheusMetricsBuilder;
-
+use uptime_lib;
 
 // Health endpoint JSON
 #[derive(Serialize)]
@@ -26,10 +25,7 @@ async fn health() -> impl Responder {
     let mut checks = vec![];
 
     // Check container uptime
-    let uptime = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
+    let uptime = uptime_lib::get().unwrap().as_secs();
     checks.push(HealthCheck {
         name: "Container uptime".to_string(),
         status: format!("{} seconds", uptime),
